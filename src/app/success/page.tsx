a65b27f8acc4797e0a0e742e10c9f371cd786d45
Ahmed59195 <168577036+Ahmed59195@users.noreply.github.com>
2025-02-08 21:17:50 +0500

    Add files via upload

---
+++ src/app/success/page.tsx
@@ -0,0 +1,10 @@
+// app/success/page.tsx
+export default function SuccessPage() {
+    return (
+      <div>
+        <h1>Payment Successful!</h1>
+        <p>Thank you for your purchase. Your payment has been processed successfully.</p>
+      </div>
+    );
+  }
+  
